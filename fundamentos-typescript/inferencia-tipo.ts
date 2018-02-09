2f3972f6a9a209c1458918f7606630e9726c2003
Rafael Marques <rafaelmarques1992@gmail.com>
2018-02-09 11:08:05 -0200

    Inclusão dos arquivos inferencia-tipo

---
+++ fundamentos-typescript/inferencia-tipo.ts
@@ -0,0 +1,11 @@
+let quantidade = 20;
+// quantidade = 'qualquer'; -> não compila
+
+let x = [1, 2, null];
+// x[0] = true; -> não compila
+
+// Contextual Typing
+window.onmousedown = function(evento) {
+    console.log(evento.button); // OK
+    // console.log(evento.buton); -> Não compila
+}
