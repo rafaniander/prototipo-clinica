2e44ed8de50f4c332e2b3d55e531daaa4580cb05
Rafael Marques <rafaelmarques1992@gmail.com>
2018-03-09 10:51:31 -0300

    Movendo o refresh token para o cookie

--- algamoney-api/src/main/java/com/example/algamoney/api/token/RefreshTokenPostProcessor.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/token/RefreshTokenPostProcessor.java
@@ -11,6 +11,7 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
@@ -27,14 +28,17 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
 	public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter returnTupe,
 			MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
 			ServerHttpRequest request, ServerHttpResponse response) {
-		
+
 		HttpServletRequest req = ((ServletServerHttpRequest) request).getServletRequest();
 		HttpServletResponse resp = ((ServletServerHttpResponse) response).getServletResponse();
-		
+
+		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) body;
+
 		String refreshToken = body.getRefreshToken().getValue();
 		adicionarRefreshTokenNoCookie(refreshToken, req, resp);
-		
-		return null;
+		removerRefreshTokenDoBory(token);
+
+		return body;
 	}
 
 	private void adicionarRefreshTokenNoCookie(String refreshToken, HttpServletRequest req, HttpServletResponse resp) {
@@ -43,6 +47,11 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
 		refreshTokenCookie.setSecure(false); // TODO: Mudar para true em producao
 		refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
 		refreshTokenCookie.setMaxAge(25920000);
+		resp.addCookie(refreshTokenCookie);
+	}
+
+	private void removerRefreshTokenDoBory(DefaultOAuth2AccessToken token) {
+		token.setRefreshToken(null);
 	}
 
 }
