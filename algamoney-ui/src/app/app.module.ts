7ab35f9e68edd6c0d5a50a7dc21f32563079b273
Rafael Marques <rafaelmarques1992@gmail.com>
2018-03-30 11:06:35 -0300

    Adicionando uma tabela de dados

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DataTableModule } from 'primeng/datatable';
 
 
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
