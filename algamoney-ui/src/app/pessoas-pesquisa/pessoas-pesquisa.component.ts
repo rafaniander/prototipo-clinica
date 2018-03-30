1d0977185c3975a0d918e88967f03b208764ef66
Rafael Marques <rafaelmarques1992@gmail.com>
2018-03-30 16:42:45 -0300

    Desafio - Criando componente de pesquisa de pessoas

---
+++ algamoney-ui/src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoas-pesquisa',
+  templateUrl: './pessoas-pesquisa.component.html',
+  styleUrls: ['./pessoas-pesquisa.component.css']
+})
+export class PessoasPesquisaComponent {
+
+  pessoas = [
+    {
+      nome: 'Rafael Marques', cidade: 'Estrela D Oeste', estado: 'SP', ativo: true
+    },
+    {
+      nome: 'Renata Marques', cidade: 'Guarulhos', estado: 'SP', ativo: true
+    },
+    {
+      nome: 'Reginaldo Marques', cidade: 'Parabapuã', estado: 'SP', ativo: true
+    },
+    {
+      nome: 'Michel Não Lembro', cidade: 'Guarulhos', estado: 'SP', ativo: false
+    },
+    {
+      nome: 'Rafael Novaes', cidade: 'Praia Grande', estado: 'SP', ativo: true
+    },
+    {
+      nome: 'Maria Betânia', cidade: 'Fernandópois', estado: 'SP', ativo: true
+    },
+    {
+      nome: 'Ricardo Ricci', cidade: 'Fernandópolis', estado: 'SP', ativo: false
+    }
+  ];
+
+}
