f7c675f61cc309ad458d86a6f7513b17b20a11d6
Rafael Marques <rafaelmarques1992@gmail.com>
2018-03-31 12:27:27 -0300

    Respondendo a eventos do hospedeiro com hostlistener

--- meuprojeto/src/app/campo-colorido.directive.ts
+++ meuprojeto/src/app/campo-colorido.directive.ts
@@ -1,5 +1,5 @@
 import { element } from 'protractor';
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
@@ -9,8 +9,16 @@ export class CampoColoridoDirective {
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2
-  ) {
+  ) { }
+
+  @HostListener('focus')
+  aoGanharFoco() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+  }
+
+  @HostListener('blur')
+  aoPerderFoco() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 
 }
