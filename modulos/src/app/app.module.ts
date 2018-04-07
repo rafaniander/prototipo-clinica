bee74c7322803def3feece0fbf56b8b2c98257f5
Rafael Marques <rafaelmarques1992@gmail.com>
2018-04-07 13:51:31 -0300

    Reexportando um módulo

--- modulos/src/app/app.module.ts
+++ modulos/src/app/app.module.ts
@@ -1,10 +1,8 @@
-import { BotoesModule } from './botoes/botoes.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { NavegacaoModule } from './navegacao/navegacao.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BotoesModule
+    NavegacaoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
