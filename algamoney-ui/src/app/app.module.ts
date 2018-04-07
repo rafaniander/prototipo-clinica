c2cab38b826f76c2a2fca31cbb4c688674182d17
Rafael Marques <rafaelmarques1992@gmail.com>
2018-04-07 14:49:25 -0300

    Desafio - Criando o feature module de pessoas

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -4,47 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
-
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { DataTableModule } from 'primeng/datatable';
-import { TooltipModule } from 'primeng/tooltip';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CalendarModule } from 'primeng/calendar';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputMaskModule } from 'primeng/inputmask';
+import { PessoasModule } from './pessoas/pessoas.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { MessageComponent } from './message/message.component';
-import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PessoasPesquisaComponent,
-    PessoaCadastroComponent,
     MessageComponent,
-    PessoasGridComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     LancamentosModule,
-    InputTextModule,
-    ButtonModule,
-    DataTableModule,
-    TooltipModule,
-    InputTextareaModule,
-    CalendarModule,
-    SelectButtonModule,
-    DropdownModule,
-    InputMaskModule
+    PessoasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ algamoney-ui/src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,36 @@
+import { CalendarModule } from 'primeng/calendar';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { InputMaskModule } from 'primeng/inputmask';
+import { DataTableModule } from 'primeng/datatable';
+import { TooltipModule } from 'primeng/tooltip';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputMaskModule,
+    DataTableModule,
+    TooltipModule,
+    InputTextModule,
+    ButtonModule
+  ],
+  declarations: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent,
+    PessoasGridComponent
+  ],
+  exports: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent
+  ]
+})
+export class PessoasModule { }
