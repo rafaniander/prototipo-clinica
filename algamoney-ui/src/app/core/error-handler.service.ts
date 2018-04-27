0c0aad922bfba16a91c18ad0b9b67d4e77af3ff0
Rafael Marques <rafaelmarques1992@gmail.com>
2018-04-27 14:23:38 -0300

    Implementação para quando o refresh token expirar

--- algamoney-ui/src/app/core/error-handler.service.ts
+++ algamoney-ui/src/app/core/error-handler.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
+import { Router } from '@angular/router';
 
 import { ToastyService } from 'ng2-toasty';
+
+import { NotAuthenticatedError } from './../seguranca/money-http';
 
 @Injectable()
 export class ErrorHandlerService {
 
-  constructor(private toasty: ToastyService) { }
+  constructor(
+    private router: Router,
+    private toasty: ToastyService
+  ) { }
 
   handle(errorResponse: any) {
     let msg: string;
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
     } else if (errorResponse instanceof Response
-        && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      && errorResponse.status >= 400 && errorResponse.status <= 499) {
       let errors;
       msg = 'Ocorreu um erro ao processar a sua solicitação';
 
