6883cda0ed5c1138f90636f943577f3ed3a49443
Rafael Marques <rafaelmarques1992@gmail.com>
2018-04-28 13:06:08 -0300

    Respondendo requisições com nodejs e express

--- algamoney-ui/src/environments/environment.prod.ts
+++ algamoney-ui/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-api-rafaniander.herokuapp.com'
+  apiUrl: 'http://localhost:8080'
+  // apiUrl: 'https://algamoney-api-rafaniander.herokuapp.com'
 };
